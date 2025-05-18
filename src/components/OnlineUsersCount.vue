71c47d8fa4a0f2b9b93ff896ccca51583508d5a9
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2025-05-18 23:24:31 +0530

    feat(ui): update OnlineUsersCount component to display online users with icon and improved styling

--- src/components/OnlineUsersCount.vue
+++ src/components/OnlineUsersCount.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Users } from "lucide-vue-next";
+
 import { usePresenceStore } from "../stores/presenceStore";
 
 const presenceStore = usePresenceStore();
@@ -6,9 +8,11 @@ const presenceStore = usePresenceStore();
 
 <template>
   <div
-    class="fixed bottom-4 left-4 bg-gray-800 bg-opacity-80 rounded-lg px-3 py-2 text-white font-mono flex items-center gap-2"
+    title="online users"
+    class="bg-green-600 rounded-lg px-4 py-2 text-white font-mono flex items-center gap-2"
   >
-    <span class="w-2 h-2 mt-1 bg-green-500 rounded-full animate-pulse"></span>
-    <div>{{ presenceStore.onlineCount }} online</div>
+    <!-- <span class="w-2 h-2 mt-1 bg-green-500 rounded-full animate-pulse"></span> -->
+    <Users class="w-4 h-4" />
+    <div>{{ presenceStore.onlineCount }}</div>
   </div>
 </template>
